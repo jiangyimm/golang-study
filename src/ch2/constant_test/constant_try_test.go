bfb29aee823cca9a61518c131e64bc4a51311a15
江毅 <903199067@qq.com>
2020-10-21 12:57:31 +0800

    ch2

---
+++ src/ch2/constant_test/constant_try_test.go
@@ -0,0 +1,29 @@
+package constant_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	Monday = iota + 1
+	Tuesday
+	Wednesday
+)
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+
+func TestConstantTry(t *testing.T) {
+	t.Log(Monday, Tuesday, Wednesday)
+	fmt.Println(Monday, Tuesday, Wednesday)
+}
+
+func TestConstantTry1(t *testing.T) {
+	a := 7 //0111
+	fmt.Println(Readable, Writable, Executable)
+	fmt.Println(a&Readable == Readable, a&Writable == Writable, a&Executable == Executable)
+}
---
+++ src/ch2/test/first_test.go
@@ -0,0 +1,11 @@
+package try_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFirstTry(t *testing.T) {
+	t.Log("My First Test")
+	fmt.Println("My First Test")
+}
